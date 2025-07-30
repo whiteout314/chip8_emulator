0731d6b1cb404918d60739e6669a37f96d252f8c
almagro-m-carlos <carlos.almagro@mymobilex.com>
2025-07-29 21:20:15 -0400

    Update core/cache/memory

--- chip8_emulator.srcs/sources_1/new/core.vhd
+++ chip8_emulator.srcs/sources_1/new/core.vhd
@@ -35,7 +35,8 @@ use IEEE.NUMERIC_STD.ALL;
 entity core is
 --  Port ( );
     Port (
-        clk : in std_logic
+        clk : in std_logic;
+        clear_screen: inout std_logic;
     );
 
 end core;
@@ -91,7 +92,7 @@ architecture Behavioral of core is
         OP_RND, OP_DRW, OP_SKP, OP_SKNP
     );
     signal state : instruction := OP_CLS;
-    --- Instruction fields ---
+    --- Instruction fields 
     signal opcode : std_logic_vector(15 downto 0) := X"0000";
     signal PC     : std_logic_vector(15 downto 0) := X"0000"; 
     signal SP     : std_logic_vector(15 downto 0) := X"0000";
@@ -100,10 +101,10 @@ architecture Behavioral of core is
     signal Y      : std_logic_vector(3 downto 0) := X"0"; 
     signal NNN    : std_logic_vector(11 downto 0) := X"000";
     signal KK     : std_logic_vector(7 downto 0) := X"00";
-    --- data path signal ---
+    --- data path signal
     signal data_path_msb : std_logic_vector(4 downto 0);
     signal data_path_lsb : std_logic_vector(4 downto 0);
-    --- Register control ---
+    --- Register control 
     signal RA_write: std_logic;
     signal RA_data_in: std_logic_vector (7 downto 0);
     signal RA_data_out: std_logic_vector (7 downto 0);
@@ -112,14 +113,14 @@ architecture Behavioral of core is
     signal RB_data_in: std_logic_vector(7 downto 0);
     signal RB_data_out: std_logic_vector(7 downto 0);
     signal RB_addr: std_logic_vector(3 downto 0);
-    -- Flag control --
+    -- Flag control
     signal flag_write: std_logic; 
     signal flag_status_in: std_logic;
     signal flag_status_out: std_logic;
-    --- Timers ---
+    --- Timers
     signal delay_timer: std_logic_vector(7 downto 0);
     signal sound_timer: std_logic_vector(7 downto 0);
-    --- Cache --- 
+    --- Cache
     signal memory_addr: std_logic_vector(15 downto 0);
     signal memory_write: std_logic;
     signal memory_data_in: std_logic_vector(7 downto 0);
@@ -139,7 +140,7 @@ begin
                     --- Fetch OP Instruction --- 
                     memory_addr <= PC;
                     if rising_edge(clk) then
-                        opcode <= shift_left(memory_data_out_msb, 8) or memory_data_out_lsb;
+                        opcode <= std_logic_vector(shift_left(unsigned(memory_data_out_msb), 8) or unsigned(memory_data_out_lsb));
                     end if;
                 when DECODE =>
                     
@@ -194,7 +195,7 @@ begin
 
                 when EXECUTE =>
                                 when EXECUTE =>
-                --- CLS [-] --- NOT IMPLEMENTED
+                --- CLS [-] --- IMPLEMENTED
                 --- RET [-] --- NOT IMPLEMENTED
                 --- SYS [-] --- NOT IMPLEMENTED
                 --- JP addr [1nnn] --- IMPLEMENTED (via data_path_msb = X"1")
@@ -210,9 +211,9 @@ begin
                 --- XOR Vx, Vy [8xy3] --- IMPLEMENTED (data_path_msb = X"8", lsb = X"3")
                 --- ADD Vx, Vy [8xy4] --- IMPLEMENTED (data_path_msb = X"8", lsb = X"4")
                 --- SUB Vx, Vy [8xy5] --- IMPLEMENTED (data_path_msb = X"8", lsb = X"5")
-                --- SHR Vx {, Vy} [8xy6] --- NOT IMPLEMENTED
+                --- SHR Vx {, Vy} [8xy6] --- IMPLEMENTED
                 --- SUBN Vx, Vy [8xy7] --- IMPLEMENTED (data_path_msb = X"8", lsb = X"7")
-                --- SHL Vx {, Vy} [8xyE] --- NOT IMPLEMENTED
+                --- SHL Vx {, Vy} [8xyE] --- IMPLEMENTED
                 --- LD I, addr [Annn] --- IMPLEMENTED (data_path_msb = X"A")
                 --- JP V0, addr [Bnnn] --- IMPLEMENTED (data_path_msb = X"B")
                 --- RND Vx, byte [Cxkk] --- NOT IMPLEMENTED
@@ -226,9 +227,11 @@ begin
                 --- ADD I, Vx [Fx1E] --- IMPLEMENTED (data_path_msb = X"F", lsb = X"1E")
                 --- LD F, Vx [Fx29] --- NOT IMPLEMENTED
                 --- LD B, Vx [Fx33] --- NOT IMPLEMENTED
-                --- LD [I], Vx [Fx55] --- NOT IMPLEMENTED
+                --- LD [I], Vx [Fx55] --- IMPLEMENTED
                 --- LD Vx, [I] [Fx65] --- NOT IMPLEMENTED
                     case state is
+                        when OP_CLS => 
+                            clear_screen <= '1';
                         when OP_SE =>
                             if RA_data_out = RB_data_out then
                                 PC <= std_logic_vector(unsigned(PC) + 2);
@@ -284,13 +287,51 @@ begin
                                             RA_data_in <= delay_timer;
                                             RA_write <= '1';
                                         when X"0A" => null;
+                                        when x"55" => 
+                                            if rising_edge(clk) then
+                                                memory_addr  <= I;
+                                                memory_data_in <= RA_data_in;
+                                                memory_write <= '1';
+                                            end if;
+                                            if rising_edge(clk) then
+                                                memory_addr  <= std_logic_vector(unsigned(I));
+                                                memory_data_in <= std_logic_vector(unsigned(RA_data_in) mod 10);
+                                                memory_write <= '1';
+                                            end if;
+                                            if rising_edge(clk) then
+                                                memory_addr  <= std_logic_vector(unsigned(I) + 1);
+                                                memory_data_in <= std_logic_vector((unsigned(RA_data_in) mod 100) / 10);
+                                                memory_write <= '1';
+                                            end if;
+                                            if rising_edge(clk) then
+                                                memory_addr  <= std_logic_vector(unsigned(I) + 2);
+                                                memory_data_in <= std_logic_vector((unsigned(RA_data_in) mod 1000) / 100);
+                                                memory_write <= '1';
+                                            end if;
                                         when X"15" => 
                                             delay_timer <= RA_data_out;
                                         when X"18" => 
                                             sound_timer <= RA_data_out;
                                         when X"9" => null; 
+                                        
                                     end case;
                             end case;
+                        when OP_SHL => 
+                            if RA_data_in & x"80" == x"10" then
+                                flag_status_in <= '1';
+                            elsif 
+                                flag_status_in <= '0';
+                            end if;
+                            flag_write <= '1';
+                            RA_data_in <= std_logic_vector(unsigned(RA_data_out) * 2); 
+                        when OP_SHR => 
+                            if RA_data_in & x"01" == x"01" then
+                                flag_status_in <= '1';
+                            elsif 
+                                flag_status_in <= '0';
+                            end if;
+                            flag_write <= '1';
+                            RA_data_in <= std_logic_vector(unsigned(RA_data_out) / 2);
                         when OP_JP => 
                             case data_path_msb is 
                                 when X"1" => PC <= NNN;
